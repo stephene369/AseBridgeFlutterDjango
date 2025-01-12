430fcc6f5aaf72f0487e06894bb7b7cf5ec283b8
stephene369 <stephenew36@gmail.com>
2025-01-12 05:58:21 +0100

    ~0,4-~4,2-~6,2 ~8,2:~10,2:~12,2

--- update.bat
+++ update.bat
@@ -6,6 +6,7 @@ for /f "tokens=2 delims==" %%A in ('"wmic os get localdatetime /value"') do set 
 :: Extraire les parties de la date et de l'heure
 set year=%datetime:~0,4%
 set month=%datetime:~4,2%
+
 set day=%datetime:~6,2%
 set hour=%datetime:~8,2%
 set minute=%datetime:~10,2%
