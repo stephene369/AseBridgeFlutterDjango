50bed330e71194b4a7c52c5691c61b6c19da8e2f
stephene369 <stephenew36@gmail.com>
2025-01-12 06:02:05 +0100

    2025--01-12-06-02-05

--- update.bat
+++ update.bat
@@ -1,10 +1,10 @@
 @echo off
 
 :: Récupérer la date et l'heure locale via PowerShell
-for /f "tokens=*" %%A in ('powershell -command "Get-Date -Format yyyyMMddHHmmss"') do set datetime=%%A
+for /f "tokens=*" %%A in ('powershell -command "Get-Date -Format yyyy--MM-dd-HH-mm-ss"') do set datetime=%%A
 
 :: Construire le message de commit
-set commit_message=Commit on %datetime%
+set commit_message=%datetime%
 
 :: Ajouter les changements, commiter et pousser
 git add .
